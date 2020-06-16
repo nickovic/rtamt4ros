eadd5dc4a6cafe4c541a9a23b3f0d42d95f07fed
TomoyaYamaguchi <tomoya.yamaguchi@toyota.com>
2020-06-16 18:20:59 -0400

    trying shift to STLIOCTSpecification(). It does not work.

--- rtamt4ros/scripts/ros_stl_monitor.py
+++ rtamt4ros/scripts/ros_stl_monitor.py
@@ -3,8 +3,12 @@ import rospy
 import sys
 import argparse
 import copy
+
+#import rtamt
+import rtamt
 from rtamt.spec.stl.specification import STLSpecification
 from rtamt.exception.stl.exception import STLParseException
+from rtamt.spec.io_stl.io_interpretation import IOInterpretation
 
 def callback(data, args):
     spec = args[0]
@@ -23,7 +27,7 @@ def monitor(stl_arg, period_arg, unit_arg):
 
     filename = stl_arg[0]
 
-    spec = STLSpecification()
+    spec = rtamt.STLIOCTSpecification()
     spec.set_sampling_period(period, unit)
     freq = spec.get_sampling_frequency()
 

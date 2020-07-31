335844f3d6538cbdcca62bd8becf55ac25d1d88c
TomoyaYamaguchi <tomoya.yamaguchi@toyota.com>
2020-07-31 12:19:56 -0400

    cleaning code

--- hsr_monitors/scripts/agent.py
+++ hsr_monitors/scripts/agent.py
@@ -6,29 +6,37 @@ from std_msgs.msg import Header
 from gazebo_msgs.msg import ModelState 
 from gazebo_msgs.srv import GetModelState, GetModelStateRequest, SetModelState
 
-rospy.init_node('agent')
-rospy.wait_for_service ('/gazebo/get_model_state')
-get_model_srv = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
-set_model_srv = rospy.ServiceProxy('/gazebo/set_model_state', SetModelState)
+freq = 10 #Hz
+speed = 0.5 #m/s
 
-odom=Odometry()
-header = Header()
-header.frame_id='/map'
+class Agent(object):
+    def __init__(self):
+        rospy.wait_for_service ('/gazebo/get_model_state')
+        self.get_model_srv = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
+        self.set_model_srv = rospy.ServiceProxy('/gazebo/set_model_state', SetModelState)
 
-model = GetModelStateRequest()
-model.model_name='unit_cylinder'
+        self.model = GetModelStateRequest()
+        self.model.model_name='unit_cylinder'
 
-r = rospy.Rate(1)
+    def control_callback(self, event):
+        result = self.get_model_srv(self.model)
+        rospy.loginfo(result)
 
-while not rospy.is_shutdown():
-    result = get_model_srv(model)
-    rospy.loginfo(result)
+        state_msg = ModelState()
+        state_msg.model_name = self.model.model_name
+        state_msg.pose = result.pose
+        state_msg.twist = result.twist
+        state_msg.pose.position.x = result.pose.position.x + speed/freq
+        resp = self.set_model_srv( state_msg )
 
-    state_msg = ModelState()
-    state_msg.model_name = model.model_name
-    state_msg.pose = result.pose
-    state_msg.twist = result.twist
-    state_msg.pose.position.x = result.pose.position.x + 0.1
-    resp = set_model_srv( state_msg )
 
-    r.sleep()
+if __name__ == '__main__':
+    # Process arguments
+    try:
+        rospy.init_node('agent')
+        agent = Agent()
+        rospy.Timer(rospy.Duration(1.0/freq), agent.control_callback)
+        rospy.spin()
+
+    except rospy.ROSInterruptException:
+        pass

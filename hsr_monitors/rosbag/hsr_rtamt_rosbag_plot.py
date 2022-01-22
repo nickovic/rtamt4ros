0c204f5a4342ef67d7c3d3aa41bcfa537bbc6bbd
Tomoya Yamaguchi <tomoya.yamaguchi@toyota.com>
2022-01-22 16:59:32 -0500

    caliblation of plot

--- hsr_monitors/rosbag/hsr_rtamt_rosbag_plot.py
+++ hsr_monitors/rosbag/hsr_rtamt_rosbag_plot.py
@@ -16,15 +16,15 @@ def colorDefinition(values):
     return color
 
 
-bagName = 'system_all_green'
-#bagName = 'planner_fail'
+#bagName = 'system_all_green'
+bagName = 'planner_fail'
 bag = rosbag.Bag( bagName + '.bag')
 #plt.rcParams.update({'font.size': 12})
 fig = plt.figure(figsize=(6, 5))
 plt.xlim([0, 45])
 plt.ylim([-0.2, 1.0])
-plt.xlabel('Time [sec]', fontsize=18)
-plt.ylabel('Robustness', fontsize=18)
+plt.xlabel('Time [sec]', fontsize=14)
+plt.ylabel('Robustness', fontsize=14)
 plt.axhspan(-10, 0, color = 'pink', alpha = 0.5)
 linewidth = 3
 

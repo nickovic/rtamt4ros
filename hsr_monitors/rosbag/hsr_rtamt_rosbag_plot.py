004b2b3926d4f0c61c2cfc7f5708b969a1bdb62b
Tomoya Yamaguchi <tomoya.yamaguchi@toyota.com>
2022-01-22 17:39:16 -0500

    clibration of plot

--- hsr_monitors/rosbag/hsr_rtamt_rosbag_plot.py
+++ hsr_monitors/rosbag/hsr_rtamt_rosbag_plot.py
@@ -1,3 +1,5 @@
+from cycler import cycler
+
 import rosbag
 import matplotlib.pyplot as plt
 
@@ -16,17 +18,20 @@ def colorDefinition(values):
     return color
 
 
-#bagName = 'system_all_green'
-bagName = 'planner_fail'
+bagName = 'system_all_green'
+#bagName = 'planner_fail'
 bag = rosbag.Bag( bagName + '.bag')
 #plt.rcParams.update({'font.size': 12})
 fig = plt.figure(figsize=(6, 5))
+# Sience+IEEE plot
+# https://github.com/garrettj403/SciencePlots/blob/master/styles/journals/ieee.mplstyle
+plt.rcParams['axes.prop_cycle'] = cycler('color', ['k', 'r', 'b', 'g']) + cycler('ls', ['-', '--', ':', '-.'])
 plt.xlim([0, 45])
 plt.ylim([-0.2, 1.0])
 plt.xlabel('Time [sec]', fontsize=14)
 plt.ylabel('Robustness', fontsize=14)
 plt.axhspan(-10, 0, color = 'pink', alpha = 0.5)
-linewidth = 3
+linewidth = 1
 
 times, values = robMessgae2TimeValueList(bag, '/rtamt/system/collEgoObs_gt')
 plt.plot(times, values, linewidth=linewidth, label='System')

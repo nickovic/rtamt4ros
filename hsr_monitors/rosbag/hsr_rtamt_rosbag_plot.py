a9c259c0a7980b740689ed9348609d23ab42a90c
Tomoya Yamaguchi <tomoya.yamaguchi@toyota.com>
2022-01-22 10:25:22 -0500

    color definition

--- hsr_monitors/rosbag/hsr_rtamt_rosbag_plot.py
+++ hsr_monitors/rosbag/hsr_rtamt_rosbag_plot.py
@@ -9,13 +9,21 @@ def robMessgae2TimeValueList(bag, topic):
         values.append(rob.value)
     return times, values
 
+def colorDefinition(values):
+    color = 'blue'
+    if min(values)<0:
+        color = 'red'
+    return color
+
+
 bagName = 'system_all_green'
 #bagName = 'planner_fail'
 bag = rosbag.Bag( bagName + '.bag')
-#plt.rcParams.update({'font.size': 12})
-fig = plt.figure(figsize=(10, 6))
-xlim=(0, 45)
-ylim=(-0.2, 1.0)
+plt.rcParams.update({'font.size': 12})
+fig = plt.figure(figsize=(6, 6))
+xlim = (0, 45)
+ylim = (-0.2, 1.0)
+linewidth = 3
 ax1 = fig.add_subplot(4, 1, 1)
 ax2 = fig.add_subplot(4, 1, 2)
 ax3 = fig.add_subplot(4, 1, 3)
@@ -23,31 +31,35 @@ ax4 = fig.add_subplot(4, 1, 4)
 
 times, values = robMessgae2TimeValueList(bag, '/rtamt/system/collEgoObs_gt')
 ax1.set(xlim=xlim, ylim=ylim)
-ax1.axhspan(-10, 0, color = "pink", alpha = 0.5)
+ax1.axhspan(-10, 0, color = 'pink', alpha = 0.5)
 ax1.tick_params(labelbottom=False)
 ax1.set_ylabel('System')
-ax1.plot(times, values, color="red", linewidth=3)
+color = colorDefinition(values)
+ax1.plot(times, values, color=color, linewidth=linewidth)
 
 times, values = robMessgae2TimeValueList(bag, '/rtamt/perception/errLoc')
 ax2.set(xlim=xlim, ylim=ylim)
-ax2.axhspan(-10, 0, color = "pink", alpha = 0.5)
+ax2.axhspan(-10, 0, color = 'pink', alpha = 0.5)
 ax2.tick_params(labelbottom=False)
 ax2.set_ylabel('Perception')
-ax2.plot(times, values, color="blue", linewidth=3)
+color = colorDefinition(values)
+ax2.plot(times, values, color=color, linewidth=linewidth)
 
 times, values = robMessgae2TimeValueList(bag, '/rtamt/planner/collGlobalPathObs')
 ax3.set(xlim=xlim, ylim=ylim)
-ax3.axhspan(-10, 0, color = "pink", alpha = 0.5)
+ax3.axhspan(-10, 0, color = 'pink', alpha = 0.5)
 ax3.tick_params(labelbottom=False)
 ax3.set_ylabel('Planner')
-ax3.plot(times, values, color="blue", linewidth=3)
+color = colorDefinition(values)
+ax3.plot(times, values, color=color, linewidth=linewidth)
 
 times, values = robMessgae2TimeValueList(bag, '/rtamt/controller/referrLocGlobalPath')
 ax4.set(xlim=xlim, ylim=ylim)
-ax4.axhspan(-10, 0, color = "pink", alpha = 0.5)
+ax4.axhspan(-10, 0, color = 'pink', alpha = 0.5)
 ax4.set_ylabel('Controller')
 ax4.set_xlabel('Time [sec]')
-ax4.plot(times, values, color="blue", linewidth=3)
+color = colorDefinition(values)
+ax4.plot(times, values, color=color, linewidth=linewidth)
 
 
 bag.close()
